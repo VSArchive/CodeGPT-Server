35a396a9af8d18b15bf0adfa373c7b0dee9b9e1c
Vineel Sai <mail@vineelsai.com>
2023-10-18 17:21:10 +0530

    add opensearch execution option

--- Code Execution/main.py
+++ Code Execution/main.py
@@ -25,85 +25,117 @@ kernel = json.loads(response.text)
 print(kernel)
 
 
+opensearch_url = (
+    "https://search-test-anqyvyqn2pxbklmlzfo27ikk4i.ap-southeast-1.es.amazonaws.com"
+)
+
+
+# @app.route("/api/run", methods=["POST"])
+# def run_code():
+#     if not os.path.exists("runs"):
+#         os.mkdir("runs")
+
+#     run_id = uuid.uuid4().hex
+#     data = request.json
+
+#     with open(f"runs/{run_id}.py", "w", encoding="utf-8") as f:
+#         f.write(data["code"])
+
+#     container = client.containers.run(
+#         image="code-execution",
+#         command=f"bash -c 'python3 /runs/{run_id}.py'",
+#         detach=True,
+#         auto_remove=True,
+#         stdout=True,
+#         stderr=True,
+#         network_mode="host",
+#         environment={"PYTHONUNBUFFERED": "1"},
+#         volumes={os.path.join(os.getcwd(), "runs"): {"bind": "/runs", "mode": "rw"}},
+#     )
+
+#     def logs():
+#         for line in container.logs(stream=True):
+#             print(line)
+#             yield line
+
+#     return Response(logs())
+
+
 @app.route("/api/execute", methods=["POST"])
 def execute_code():
-    # if not os.path.exists("runs"):
-    #     os.mkdir("runs")
-
-    # with open(f"runs/{run_id}.py", "w", encoding="utf-8") as f:
-    #     f.write(data["code"])
-
-    # container = client.containers.run(
-    #     image="code-execution",
-    #     command=f"bash -c 'python3 /runs/{run_id}.py'",
-    #     detach=True,
-    #     auto_remove=True,
-    #     stdout=True,
-    #     stderr=True,
-    #     network_mode="host",
-    #     environment={"PYTHONUNBUFFERED": "1", "DISPLAY": "10.0.0.50:0.0"},
-    #     volumes={os.path.join(os.getcwd(), "runs"): {"bind": "/runs", "mode": "rw"}},
-    # )
-
-    # def logs():
-    #     for line in container.logs(stream=True):
-    #         print(line)
-    #         yield line
-
-    # return Response(logs())
-
     data = request.json
     print(data)
 
     # Load the notebook and get the code of each cell
     code = data["code"]
+    lang = data["language"]
 
-    # Execution request/reply is done on websockets channels
-    ws = create_connection(
-        "ws://13.215.50.249:8888/api/kernels/" + kernel["id"] + "/channels",
-        header=headers,
-    )
+    if lang == "python":
+        # Execution request/reply is done on websockets channels
+        ws = create_connection(
+            "ws://13.215.50.249:8888/api/kernels/" + kernel["id"] + "/channels",
+            header=headers,
+        )
 
-    def send_execute_request(code):
-        msg_type = "execute_request"
-        content = {"code": code, "silent": False}
-        hdr = {
-            "msg_id": uuid.uuid4().hex,
-            "session": "test",
-            "data": datetime.datetime.now().isoformat(),
-            "msg_type": msg_type,
-        }
-        msg = {"header": hdr, "parent_header": hdr, "metadata": {}, "content": content}
+        def send_execute_request(code):
+            msg_type = "execute_request"
+            content = {"code": code, "silent": False}
+            hdr = {
+                "msg_id": uuid.uuid4().hex,
+                "session": "test",
+                "data": datetime.datetime.now().isoformat(),
+                "msg_type": msg_type,
+            }
+            msg = {
+                "header": hdr,
+                "parent_header": hdr,
+                "metadata": {},
+                "content": content,
+            }
 
-        return msg
+            return msg
 
-    ws.send(json.dumps(send_execute_request(code)))
+        ws.send(json.dumps(send_execute_request(code)))
 
-    # output = []
-    def logs():
-        msg_type = ""
-        rsp = {
-            "content": {"execution_state": ""},
-        }
-        while msg_type != "stream":
-            rsp = json.loads(ws.recv())
-            msg_type = rsp["msg_type"]
+        # output = []
+        def logs():
+            msg_type = ""
+            rsp = {
+                "content": {"execution_state": ""},
+            }
+            while msg_type != "stream":
+                rsp = json.loads(ws.recv())
+                msg_type = rsp["msg_type"]
 
-            print(json.dumps(rsp, indent=4))
+                print(json.dumps(rsp, indent=4))
 
-            if msg_type == "stream":
-                yield rsp["content"]["text"]
-            elif msg_type == "display_data":
-                yield rsp["content"]["data"]["image/png"]
-            elif msg_type == "error":
-                for t in rsp["content"]["traceback"]:
-                    yield t
-            elif msg_type == "status":
-                yield rsp["content"]["execution_state"]
-            else:
-                yield "Unknown message type: " + msg_type
+                if msg_type == "stream":
+                    yield rsp["content"]["text"]
+                elif msg_type == "display_data":
+                    yield rsp["content"]["data"]["image/png"]
+                elif msg_type == "error":
+                    for t in rsp["content"]["traceback"]:
+                        yield t
+                elif msg_type == "status":
+                    yield rsp["content"]["execution_state"]
+                else:
+                    yield "Unknown message type: " + msg_type
 
-        # ws.close()
+            ws.close()
+
+    elif lang == "sql":
+        res = requests.post(
+            opensearch_url + "/_plugins/_sql",
+            json={"query": code},
+            headers={
+                "Content-Type": "application/json",
+                "Authorization": "Basic dGVzdDpUZXN0QDEyMw==",
+            },
+            timeout=10,
+        )
+
+        def logs():
+            yield res.json()
 
     # return output
     return Response(logs())
